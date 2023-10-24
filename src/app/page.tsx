b7750fbee430c2d4339d76fb6e0c72a7b6f8c48d
wthoutjc <juancamilorr2012@hotmail.com>
2023-10-24 14:04:21 -0500

    welcome

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,10 @@ export default function Home() {
         Welcome to Currency App
       </Typography>
       <Divider />
+      <Typography variant="body2" sx={{ mb: 2 }}>
+        My name is Juan Camilo Ramírez Rátiva and I&apos;m a AWS Fullstack
+        Developer.
+      </Typography>
     </>
   );
 }
